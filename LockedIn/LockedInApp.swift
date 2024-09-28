e0cd1935ceaeb3f88c5bbfb22c9a403241aea5cd
Anthony Delgado <115326818+AnthonyD4101@users.noreply.github.com>
2024-09-27 22:04:32 -0500

    Initial Commit

---
+++ LockedIn/LockedInApp.swift
@@ -0,0 +1,17 @@
+//
+//  LockedInApp.swift
+//  LockedIn
+//
+//  Created by Anthony Delgado on 9/27/24.
+//
+
+import SwiftUI
+
+@main
+struct LockedInApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LockedInTests/LockedInTests.swift
@@ -0,0 +1,17 @@
+//
+//  LockedInTests.swift
+//  LockedInTests
+//
+//  Created by Anthony Delgado on 9/27/24.
+//
+
+import Testing
+@testable import LockedIn
+
+struct LockedInTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
