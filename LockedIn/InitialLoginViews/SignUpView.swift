31ebcae964ea3324254163fa41bada47533c2fc7
Anthony Delgado <115326818+AnthonyD4101@users.noreply.github.com>
2024-10-18 20:27:50 -0500

    Create SignUpView, push new branch off main for sign in and sign up views

---
+++ LockedIn/InitialLoginViews/SignUpView.swift
@@ -0,0 +1,18 @@
+//
+//  SignUpView.swift
+//  LockedIn
+//
+//  Created by Anthony Delgado on 10/18/24.
+//
+
+import SwiftUI
+
+struct SignUpView: View {
+    var body: some View {
+        Text("Sign Up View")
+    }
+}
+
+#Preview {
+    SignUpView()
+}
