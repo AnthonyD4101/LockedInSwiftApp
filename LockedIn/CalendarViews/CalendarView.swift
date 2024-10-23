20db1e4692bcee3072118e6b9da50533cde4019c
Anthony Delgado <115326818+AnthonyD4101@users.noreply.github.com>
2024-10-22 23:45:06 -0500

    DatePicker implementation on CalendarView

--- LockedIn/CalendarViews/CalendarView.swift
+++ LockedIn/CalendarViews/CalendarView.swift
@@ -8,14 +8,23 @@
 import SwiftUI
 
 struct CalendarView: View {
+    @State private var selectedDate = Date()
+    
     var body: some View {
-        ZStack {
-            Color(.black)
-                .edgesIgnoringSafeArea(.all)
+        VStack {
+            DatePicker(
+                "Select a date",
+                selection: $selectedDate,
+                displayedComponents: .date
+            )
+            .datePickerStyle(GraphicalDatePickerStyle())
+            .padding(.top, -50)
+            .frame(maxHeight: UIScreen.main.bounds.height / 2)
+            .preferredColorScheme(.dark)
             
-            Text("Calendar View")
-                .foregroundColor(.white)
+            Spacer()
         }
+        .background(Color.black.edgesIgnoringSafeArea(.all))
     }
 }
 
