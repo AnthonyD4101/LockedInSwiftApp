6504f8ee2e8b6d3cb10821f3e467206c8b972c12
Anthony Delgado <115326818+AnthonyD4101@users.noreply.github.com>
2024-10-24 03:04:38 -0500

    Account&SettingsView: setup profile screen welcome

--- LockedIn/ProfileViews/ProfileView.swift
+++ LockedIn/ProfileViews/ProfileView.swift
@@ -8,17 +8,48 @@
 import SwiftUI
 
 struct ProfileView: View {
+    @EnvironmentObject var userViewModel: UserViewModel
+    @EnvironmentObject var taskViewModel: TaskViewModel
+    
     var body: some View {
         ZStack {
             Color(.black)
                 .edgesIgnoringSafeArea(.all)
             
-            Text("Profile View")
-                .foregroundColor(.white)
+            VStack {
+                // Account Icon
+                Image(systemName: "person.circle.fill")
+                    .resizable()
+                    .frame(width: 100, height: 100)
+                    .foregroundColor(.white)
+                    .padding(.top, 40)
+                
+                // Welcome Message
+                if let username = userViewModel.currentUser?.username {
+                    Text("Welcome, \(username)!")
+                        .foregroundColor(.white)
+                        .font(.system(size: 24))
+                        .bold()
+                        .padding(.top, 16)
+                } else {
+                    Text("Welcome!")
+                        .foregroundColor(.white)
+                        .font(.system(size: 24))
+                        .bold()
+                        .padding(.top, 16)
+                }
+                
+                Spacer()
+            }
         }
     }
 }
 
 #Preview {
+    let userViewModel = UserViewModel()
+    let taskViewModel = TaskViewModel()
+    
     ProfileView()
+        .environmentObject(userViewModel)
+        .environmentObject(taskViewModel)
 }
